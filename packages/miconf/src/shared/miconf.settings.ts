926b245fdff65e9240bd37f50f15feaac3855c97
eonae <eonae.white@gmail.com>
2021-02-13 22:44:15 +0300

    feat: add passing ajv options via miconf.config

--- packages/miconf/src/shared/miconf.settings.ts
+++ packages/miconf/src/shared/miconf.settings.ts
@@ -1,15 +1,25 @@
+import { Options } from 'ajv';
 import { join, isAbsolute } from 'path';
 import { readObj } from '@eonae/common';
 import { SemanticVersion } from '@eonae/semantic-version';
 
+export interface MiConfSettingsRaw {
+  supported: string[],
+  ajv: Options
+}
+
 export class MiConfSettings {
   constructor (
-    public readonly supported: SemanticVersion[]
+    public readonly supported: SemanticVersion[],
+    public readonly ajv: Options
   ) { }
 
   public static async load (path = 'miconf.config.yml'): Promise<MiConfSettings> {
     const fullpath = isAbsolute(path) ? path : join(process.cwd(), path);
-    const content = await readObj(fullpath) as { supported: string[] };
-    return new MiConfSettings(content.supported.map(x => new SemanticVersion(x)));
+    const content = await readObj(fullpath) as MiConfSettingsRaw;
+    return new MiConfSettings(
+      content.supported.map(x => new SemanticVersion(x)),
+      content.ajv
+    );
   }
 }
